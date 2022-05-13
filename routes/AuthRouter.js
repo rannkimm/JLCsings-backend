4ad8fa4ab34b102647f23ae444ed19aabaed3b6e
Ran Kim <rannio@gmail.com>
2022-05-13 15:43:58 -0400

    debugged auth

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,11 +1,15 @@
 //////////// IMPORT /////////////
 const router = require('express').Router()
 const controller = require('../controllers/AuthController')
+const middleware = require('../middleware')
 
 /////////// SET AUTHCONTROLLER ROUTES ////////////
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
-router.get('/session', controller.CheckSession)
+router.get('/session', 
+middleware.stripToken,
+middleware.verifyToken,
+controller.CheckSession)
 
 ///////// EXPORT /////////
 module.exports = router
