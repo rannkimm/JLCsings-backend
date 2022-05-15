75e4e30636559992181578af06a8036bf4da238d
Ran Kim <rannio@gmail.com>
2022-05-15 02:47:48 -0400

    created seed for musicsheet

--- seeders/20220511024802-seed-data-musicsheet.js
+++ seeders/20220511024802-seed-data-musicsheet.js
@@ -6,18 +6,40 @@ module.exports = {
       'musicsheets',
       [
         {
-          user_id: 18,
+          music_id: 13,
           title: '왕이신 나의 하나님',
-          category: ['입례송', '느린송', '기도송', '축도송'],
-          video: 'https://www.youtube.com/embed/CaIKkfEhEOY',
-          description: '원래 F-key 찬양입니다. G-key 와 A-key 악보도 있습니다!'
+          image: 'http://www.fellowshipusa.com/xe/note/files/attach/images/175798/157/177/912.jpg',
+          key: 'F'
         },
         {
-          user_id: 18,
+          music_id: 13,
+          title: '왕이신 나의 하나님',
+          image: 'http://pds113.cafe.daum.net/image/8/cafe/2009/08/06/09/58/4a7a2ac17f75a',
+          key: 'G'
+        },
+        {
+          music_id: 14,
           title: '내 손을 주께 높이 듭니다',
-          category: ['입례송', '느린송', '기도송'],
-          video: 'https://www.youtube.com/embed/-hatwOlbYTM',
-          description: '원래 G-key 찬양입니다. F-key 와 A-key 악보도 있습니다!'
+          image: 'https://t1.daumcdn.net/cfile/blog/12566C0C499268AA15',
+          key: 'G'
+        },
+        {
+          music_id: 14,
+          title: '내 손을 주께 높이 듭니다',
+          image: 'https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfNzkg/MDAxNjM5Mjc4NDU4MzM0.Dhlq8P-e9vRKIklsKdKkMHv6nIUOXHwv-1tqXMPkXMkg.glRH4vj7o6eDJ7QaSDWy4cSlum2fxqakJRhG6bPnw1Yg.PNG.0909junseo/%25EB%2582%25B4_%25EC%2586%2590%25EC%259D%2584_%25EC%25A3%25BC%25EA%25BB%2598_%25EB%2586%2592%25EC%259D%25B4_%25EB%2593%25AD%25EB%258B%2588%25EB%258B%25A4_(A)-1.png?type=w800',
+          key: 'A'
+        },
+        {
+          music_id: 14,
+          title: '내 손을 주께 높이 듭니다',
+          image: 'https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfMjgw/MDAxNjM5Mjc4NDU4MDYz.xiDDe167glAQKKtx28Z0frkt75ZmfYuAxZYLoBO-wDUg.XLT2Q4T3y8S3Dq9g6DRYwlid0kKLvQwGdymp9OpbgDgg.PNG.0909junseo/%25EB%2582%25B4_%25EC%2586%2590%25EC%259D%2584_%25EC%25A3%25BC%25EA%25BB%2598_%25EB%2586%2592%25EC%259D%25B4_%25EB%2593%25AD%25EB%258B%2588%25EB%258B%25A4_(F)-1.png?type=w800',
+          key: 'F'
+        },
+        {
+          music_id: 18,
+          title: '당신은 하나님의 언약안에',
+          image: 'https://postfiles.pstatic.net/data41/2009/3/8/251/345_rawonjae.jpg?type=w3',
+          key: 'D'
         }
       ]
     )
